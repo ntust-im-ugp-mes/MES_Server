270f72b4a0511632a0e72449e46ab82892bb6281
AmazingOrionArm <s990401@gmail.com>
2018-12-14 23:50:55 +0800

    test

--- mes_backend_181012/settings.py
+++ mes_backend_181012/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/file_static/'
-STATICS_ROOT = os.path.join(BASE_DIR,'file_static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'file_static')]
+STATIC_URL = '/static/'
+STATICS_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
