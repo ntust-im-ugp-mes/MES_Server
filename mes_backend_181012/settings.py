e177d1e8b01dbf879f4b8a671782dcf1d994475f
AmazingOrionArm <s990401@gmail.com>
2018-12-14 23:41:03 +0800

    Revert "repair static folder"
    This reverts commit ddb47ded8ef552ec9d9801bb3a92179b63f52766.

--- mes_backend_181012/settings.py
+++ mes_backend_181012/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/statics/'
-STATICS_ROOT = os.path.join(BASE_DIR,'statics')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'statics')]
+STATIC_URL = '/static/'
+STATICS_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
